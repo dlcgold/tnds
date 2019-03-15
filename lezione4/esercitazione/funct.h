a68a703642b25cb78809ad7b07502cd87a9c063d
dlcgold <dlcgold96@gmail.com>
2019-03-15 12:00:13 +0100

    inizio lezione 4

--- lezione4/esercitazione/funct.h
+++ lezione4/esercitazione/funct.h
@@ -5,4 +5,24 @@
 #ifndef ESERCITAZIONE_FUNCT_H
 #define ESERCITAZIONE_FUNCT_H
 
+#include <vector>
+#include <string>
+#include <map>
+#include "comune.h"
+#include "TGraph.h"
+
+using namespace std;
+
+struct OverallQuantities {
+    double mean;
+    int num;
+};
+
+OverallQuantities CalcolaMedieTotali ( const vector<comune> & ListaComuni );
+
+TGraph CalcolaDistribuzioneEta ( const vector<comune> & ListaComuni );
+
+vector<comune> ReadFromFile ( string filename ) ;
+
+void PrintStat(const OverallQuantities &) ;
 #endif //ESERCITAZIONE_FUNCT_H
